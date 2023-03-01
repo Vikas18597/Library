e399b3e29c4daa219e0fdca74a434ec69b8efcb8
Vikas <khativikas97@gmail.com>
2023-03-02 03:32:51 +0530

    fix bug for error message for flag and pages

--- lib.js
+++ lib.js
@@ -20,41 +20,96 @@ addBookToLibrary(book2)
 const book3 = new Book('Viwas', 'Medium Brain', '700', 'No')
 addBookToLibrary(book3)
 
-// creating table based on book length
-for (let i = 0; i < myLibrary.length + 1; i++) {
-  const heading = document.createElement('tr')
-  heading.setAttribute('id', 'row-' + i)
-  const element = document.getElementById('table')
-  element.appendChild(heading)
-}
+function Printdata () {
+  // deleting the section
+  const temp = document.getElementById('table')
+  temp.innerHTML = ''
+  // creating table based on book length
+  for (let i = 0; i < myLibrary.length + 1; i++) {
+    const heading = document.createElement('tr')
+    heading.setAttribute('id', 'row-' + i)
+    const element = document.getElementById('table')
+    element.appendChild(heading)
+  }
 
-for (const keys in book1) {
-  const heading = document.createElement('th')
-  const text = document.createTextNode(keys)
-  heading.appendChild(text)
-  const element = document.getElementById('row-0')
-  element.appendChild(heading)
-}
+  for (const keys in book1) {
+    const heading = document.createElement('th')
+    const text = document.createTextNode(keys)
+    heading.appendChild(text)
+    const element = document.getElementById('row-0')
+    element.appendChild(heading)
+  }
 
-for (let i = 0; i < myLibrary.length; i++) {
-  const temp = Object.values(myLibrary[i])
-  for (let j = 1; j < Object.values(temp).length + 1; j++) {
-    const cell = document.createElement('td')
-    const text = document.createTextNode(temp[j - 1])
-    cell.appendChild(text)
-    const element = document.getElementById('row-' + (i + 1))
-    element.appendChild(cell)
+  for (let i = 0; i < myLibrary.length; i++) {
+    const temp = Object.values(myLibrary[i])
+    for (let j = 1; j < Object.values(temp).length + 1; j++) {
+      const cell = document.createElement('td')
+      const text = document.createTextNode(temp[j - 1])
+      cell.appendChild(text)
+      const element = document.getElementById('row-' + (i + 1))
+      element.appendChild(cell)
+    }
   }
 }
 
-// adding a function for pop up box with details for entires of books
+// calling it once
+Printdata()
 
+// adding a function for pop up box with details for entires of books
 const popUp = document.querySelector('.open-button')
-popUp.addEventListener('click', function () {
+popUp.addEventListener('click', function (e) {
   document.getElementById('myForm').style.display = 'block'
+  e.preventDefault()
 })
 
 const popOut = document.querySelector('.Enter')
-popOut.addEventListener('click', function () {
-  document.getElementById('myForm').style.display = 'none'
+popOut.addEventListener('click', (e) => {
+  e.preventDefault()
+  const newAuthor = document.getElementById('Name-author').value
+
+  const newTitle = document.getElementById('Book-title').value
+
+  const newPages = document.getElementById('Book-pages').value
+
+  const readFlag = document.getElementsByName('Book-readFlag')
+  console.log(!newPages)
+  // for checking if there are non empty values
+  if (newAuthor && newTitle && newPages) {
+    if (readFlag[0].checked) {
+      const booknew = new Book(newAuthor, newTitle, newPages, 'Yes')
+      addBookToLibrary(booknew)
+      document.getElementById('myForm').style.display = 'none'
+      Printdata()
+      console.log(myLibrary)
+      document.querySelector('.form-container').reset()
+    } else if (readFlag[1].checked) {
+      const booknew = new Book(newAuthor, newTitle, newPages, 'No')
+      addBookToLibrary(booknew)
+      document.getElementById('myForm').style.display = 'none'
+      Printdata()
+      console.log(myLibrary)
+      document.querySelector('.form-container').reset()
+    }
+  } else {
+    if (!newAuthor) {
+      document.getElementById('empty-author').style.display = 'block'
+    } else {
+      document.getElementById('empty-author').style.display = 'none'
+    }
+    if (!newTitle) {
+      document.getElementById('empty-title').style.display = 'block'
+    } else {
+      document.getElementById('empty-title').style.display = 'none'
+    }
+    if (!newPages) {
+      document.getElementById('empty-page').style.display = 'block'
+    } else if (newPages) {
+      document.getElementById('empty-page').style.display = 'none'
+    }
+    if (!readFlag[0].checked || !readFlag[1].checked) {
+      document.getElementById('empty-flag').style.display = 'block'
+    } else {
+      document.getElementById('empty-flag').style.display = 'none'
+    }
+  }
 })
